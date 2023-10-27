b0bb2fa85f3ebeeb9db58d2469420b9d032f3c92
davidmoina <luisdavidmoina@gmail.com>
2023-10-27 23:22:20 +0200

    ✨ Add seed resource to insert products in DB

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+
+  async executeSeed() {
+    await this.insertNewProducts();
+
+    return 'Seed executed';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = [];
+
+    products.forEach((product) => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
+    return true;
+  }
+}
