93dd567992b0bd2e5ad1a24e7ff6507196ea891b
davidmoina <luisdavidmoina@gmail.com>
2023-10-27 22:56:41 +0200

    🚧 Delete on cascade & deleteAllProducts method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -147,4 +147,14 @@ export class ProductsService {
       'Unexpected error, check server logs',
     );
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
 }
